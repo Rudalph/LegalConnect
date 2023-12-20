9bc220ad74b421522a642380a6df421d4be25502
Rudalph <gonsalvesrudalph@gmail.com>
2023-12-20 06:46:16 +0530

    static push

---
+++ app/staticStoring/page.jsx
@@ -0,0 +1,20 @@
+// pages/api/firestoreAPI.js
+
+import { db } from '../../path-to-your-firebase-config'; // Import your Firebase configuration
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { userId, newData } = req.body; // Assuming you send userId and newData in the request body
+
+    try {
+      const userRef = db.collection('users').doc(userId);
+      await userRef.set(newData);
+      res.status(200).json({ message: 'Data updated successfully' });
+    } catch (error) {
+      console.error('Error updating data:', error);
+      res.status(500).json({ error: 'Failed to update data' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}
